95926a3c96986a960dce636b30fb539987ee655c
bohdanshev <59934436+bohdanshev@users.noreply.github.com>
2020-09-02 18:48:28 +0300

    Update sales_by_manager.sql

--- sales_by_manager.sql
+++ sales_by_manager.sql
@@ -1,15 +1,44 @@
-select p.person, sum(o.sales) as sales_region
-from people p
-join orders o
-on p.region = o.region
-where p.region = o.region
-group by p.person 
-order by sales_region desc
 
-select p.person, sum(o.sales) as sales_region
+select p.person, round(sum(o.sales)) as sales_manager_total
+from orders o
+join people p
+on p.region = o.region
+group by p.person 
+order by sales_manager_total desc
+;
+
+select p.person, round(sum(o.sales)) as sales_manager_2016
+from orders o 
+join people p
+on o.region = p.region
+where extract('year' from o.order_date) = 2016
+group by p.person
+order by sales_manager_2016 desc
+;
+
+select p.person, round(sum(o.sales)) as sales_manager_2017
+from orders o 
+join people p
+on o.region = p.region
+where extract('year' from order_date) = 2017
+group by p.person
+order by sales_manager_2017 desc
+;
+
+select p.person, round(sum(o.sales)) as sales_manager_2018
 from people p
 join orders o
 on p.region = o.region
-where p.region = o.region
+where extract ('year' from o.order_date) = 2018
 group by p.person 
-order by sales_region desc
+order by sales_manager_2018 desc
+;
+
+select p.person, round(sum(o.sales)) as sales_manager_2019 
+from orders o 
+join people p
+on o.region = p.region 
+where extract('year' from o.order_date) = 2019
+group by p.person 
+order by sales_manager_2019 desc
+;

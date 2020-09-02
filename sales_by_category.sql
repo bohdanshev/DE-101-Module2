73fd453637a0e05d56e6db948350072e7eec9a4b
bohdanshev <59934436+bohdanshev@users.noreply.github.com>
2020-09-02 19:25:15 +0300

    Update sales_by_category.sql

--- sales_by_category.sql
+++ sales_by_category.sql
@@ -1,18 +1,26 @@
-select
-subcategory, count(distinct order_id) as orders_num
+select distinct category, count(distinct order_id) as orders_num, round(sum(sales)) as sales_sum
+from orders
+group by category 
+order by sales_sum desc
+;
+
+select distinct subcategory as office_supplies, count(distinct order_id) as orders_num, round(sum(sales)) as sales_sum
 from orders o
 where o.category = 'Office Supplies'
-group by subcategory 
-order by orders_num desc
+group by office_supplies 
+order by sales_sum desc
 ;
-select subcategory, count(distinct order_id) as orders_num
+
+select distinct subcategory as technology, count(distinct order_id) as orders_num, round(sum(sales)) as sales_sum
 from orders o 
 where o.category = 'Technology'
-group by subcategory 
-order by orders_num desc
+group by technology 
+order by sales_sum desc
 ;
-select subcategory, count(distinct order_id) as orders_num
+
+select distinct subcategory as furniture, count(distinct order_id) as orders_num, round(sum(sales)) as sales_sum
 from orders o
 where o.category = 'Furniture'
-group by subcategory 
-order by orders_num desc
+group by furniture 
+order by sales_sum desc
+;
